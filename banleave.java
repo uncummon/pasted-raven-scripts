2383bf71a44569c713b99a9051dff967234b020a
uncommon <78572648+uncummon@users.noreply.github.com>
2024-11-09 12:44:53 +0100

    Create banleave.java

---
+++ banleave.java
@@ -0,0 +1,12 @@
+boolean justSentCommand = false;
+
+boolean onChat(String msg) {
+    if (msg.contains("A player has been removed from your game.")) {
+        client.chat("/l");
+        client.print("&eYou have been automatically disconnected due to a ban.");
+        justSentCommand = true;
+        return true;
+    }
+    justSentCommand = false;
+    return true;
+}
