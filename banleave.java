4f77f57d5abcfb636b9c8106cc1421c1a6c05e19
uncommon <78572648+uncummon@users.noreply.github.com>
2024-11-09 13:06:14 +0100

    automatically leaves if someone in your lobby is banned

--- banleave.java
+++ banleave.java
@@ -7,7 +7,6 @@ boolean justSentCommand = false;
 boolean onChat(String msg) {
     if (msg.contains("A player has been removed from your game.")) {
         client.chat("/l");
-        client.sleep(500);
         client.print("&eYou have been automatically disconnected due to a ban.");
         justSentCommand = true;
         return true;
